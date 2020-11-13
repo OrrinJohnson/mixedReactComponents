e2e3ae96360ffe9eadd8832dcd4970a1992213a5
OrrinJohnson <orrin24johnson@gmail.com>
2020-11-13 15:33:26 -0700

    Menu component

---
+++ menuAndHeart/Menu.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+class Menu extends Component {
+    state = {
+        show: true
+    }
+
+    toggleMenu = () => {
+        this.setState(prevState => {
+            return {
+                show: !prevState.show
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.toggleMenu}>
+                    {this.state.show ? "Hide Menu" : "Show Menu"}
+                    Menu
+                </button>
+                <nav style={{display: this.state.show ? "block" : "none"}}>
+                    <h6>You are signed in as anonymous</h6>
+                    <a>View your profile</a>
+                    <a>Your Posts</a>
+                    <a>Other's Posts</a>
+                    <a>Favorites</a>
+                </nav>
+            </div>
+        )
+    }
+}
+
+export default Menu
